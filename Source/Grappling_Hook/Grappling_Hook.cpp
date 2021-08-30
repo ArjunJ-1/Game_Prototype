232c88e5108e0d2a5c256eace7f01e54f9885ce1
ArjunJ-1 <ar.jain0000@gmail.com>
2021-08-30 15:14:20 +0200

    Delete

--- Source/Grappling_Hook/Grappling_Hook.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Grappling_Hook.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Grappling_Hook, "Grappling_Hook" );
- 
